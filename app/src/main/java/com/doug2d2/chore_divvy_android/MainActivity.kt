016fb1175b8c2dca3125b1567e48184384fd7704
Matt Vella <mattvella07@gmail.com>
2020-03-04 20:38:44 -0600

    Initial commit

---
+++ app/src/main/java/com/doug2d2/chore_divvy_android/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.doug2d2.chore_divvy_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
