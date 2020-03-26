8c24623fac93c7f5a5b696ce1d24230eccd5ec6a
Matt Vella <mattvella07@gmail.com>
2020-03-26 16:53:06 -0500

    Added login fragment

--- app/src/main/java/com/doug2d2/chore_divvy_android/user/LoginFragment.kt
+++ app/src/main/java/com/doug2d2/chore_divvy_android/user/LoginFragment.kt
@@ -1,18 +1,16 @@
 package com.doug2d2.chore_divvy_android.user
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
-
 import com.doug2d2.chore_divvy_android.R
-import com.doug2d2.chore_divvy_android.database.ChoreDivvyDatabase
 import com.doug2d2.chore_divvy_android.databinding.FragmentLoginBinding
 import timber.log.Timber
 
@@ -45,34 +43,69 @@ class LoginFragment : Fragment() {
             this, viewModelFactory).get(LoginViewModel::class.java)
         binding.viewModel = loginViewModel
 
-        // LOADING, SUCCESS, INVALID_CREDENTIALS, CONNECTION_ERROR, OTHER_ERROR
+        loginViewModel.username.observe(viewLifecycleOwner, Observer<String> { username ->
+            if (username != "" && loginViewModel.password.value != null && loginViewModel.password.value != "") {
+                binding.loginButton.isEnabled = true
+            } else {
+                binding.loginButton.isEnabled = false
+            }
+        })
+
+        loginViewModel.password.observe(viewLifecycleOwner, Observer<String> { password ->
+            if (password != "" && loginViewModel.username.value != null && loginViewModel.username.value != "") {
+                binding.loginButton.isEnabled = true
+            } else {
+                binding.loginButton.isEnabled = false
+            }
+        })
+
         loginViewModel.loginStatus.observe(viewLifecycleOwner, Observer<LoginStatus> { loginStatus ->
             when (loginStatus) {
                 LoginStatus.LOADING -> {
                     Timber.i("Loading...")
-                    binding.errorText.visibility = View.INVISIBLE
+                    binding.errorText.visibility = View.GONE
+                    binding.loginButton.isEnabled = false
                 }
                 LoginStatus.SUCCESS -> {
                     findNavController().navigate(R.id.action_loginFragment_to_choreListFragment)
-                    binding.errorText.visibility = View.INVISIBLE
+                    binding.errorText.visibility = View.GONE
+                    binding.loginButton.isEnabled = true
                 }
                 LoginStatus.INVALID_CREDENTIALS -> {
                     Timber.i("Incorrect username and/or password")
                     binding.errorText.text = "Incorrect username and/or password"
                     binding.errorText.visibility = View.VISIBLE
+                    binding.loginButton.isEnabled = true
                 }
                 LoginStatus.CONNECTION_ERROR -> {
                     Timber.i("Connection Error")
                     binding.errorText.text = "Connection Error"
                     binding.errorText.visibility = View.VISIBLE
+                    binding.loginButton.isEnabled = true
                 }
                 LoginStatus.OTHER_ERROR -> {
                     Timber.i("Other Error")
                     binding.errorText.text = "Other Error"
                     binding.errorText.visibility = View.VISIBLE
+                    binding.loginButton.isEnabled = true
                 }
             }
         })
+
+        // Call onLogin if Enter is pressed from the password edit text
+        binding.passwordEditText.setOnKeyListener { v, keyCode, event ->
+            if (event.action === KeyEvent.ACTION_DOWN) {
+                when (keyCode) {
+                    KeyEvent.KEYCODE_DPAD_CENTER, KeyEvent.KEYCODE_ENTER -> {
+                        loginViewModel.onLogin()
+                        true
+                    }
+                    else -> false
+                }
+            }
+            false
+        }
+
 
         return binding.root
     }
